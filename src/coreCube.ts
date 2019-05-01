8768fc5773ac2e297d0eb83ce12004515220ef7c
Ikuo Terado <eqobar@gmail.com>
2019-05-01 19:52:59 +0900

    Initial commit

---
+++ src/coreCube.ts
@@ -0,0 +1,41 @@
+import Webble from 'webble'
+
+class CoreCube {
+  peripheral: any
+
+  public static async discover() {
+    const peripheral = await Webble.discover(
+      '10b20100-5b3b-4571-9508-cf3efcd7bbae'
+    )
+    if (!peripheral) {
+      return
+    }
+
+    await peripheral.connect()
+
+    return new CoreCube(peripheral)
+  }
+
+  constructor(peripheral) {
+    this.peripheral = peripheral
+  }
+
+  public move(speeds: number[], duration: number) {
+    const data = []
+
+    data.push(duration ? 2 : 1)
+
+    for (let i = 0; i < speeds.length; i++) {
+      const speed = Math.max(Math.min(speeds[i], 100), -100)
+      data.push(i + 1, speed >= 0 ? 1 : 2, Math.abs(speed))
+    }
+
+    if (duration) {
+      data.push(duration * (1000 / 10))
+    }
+
+    this.peripheral.write('10b20102-5b3b-4571-9508-cf3efcd7bbae', data, false)
+  }
+}
+
+export default CoreCube
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import CoreCube from './coreCube'
+
+const element = document.getElementById('root')
+if (element) {
+  element.innerHTML = `
+<div>
+  <button id="button">Discover</button>
+  <button id="forward">Forward</button>
+  <button id="backward">Backward</button>
+</div>
+`
+
+  document.querySelector('#button').addEventListener('click', async () => {
+    // const Cube = window.CoreCube;
+    const cube = await CoreCube.discover()
+
+    document.querySelector('#forward').addEventListener('click', () => {
+      cube.move([70, 70], 500)
+    })
+
+    document.querySelector('#backward').addEventListener('click', () => {
+      cube.move([-70, -70], 500)
+    })
+  })
+}
